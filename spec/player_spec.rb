b9c6d30ae2110d3d6713aacff62abfc1d6eb2304
semliko <megabytus@gmail.com>
2021-04-13 17:24:17 +0100

    remove spec

--- spec/player_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe Player do
-  let(:player) { Player.new(name: 'Bill', balance: 100) }
-
-  it 'has a name' do
-    expect(player).respond_to?(:name)
-  end
-end
